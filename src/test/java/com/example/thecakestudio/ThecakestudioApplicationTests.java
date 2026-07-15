9439f29e95943518e2bb902ea3576cceb1a852a0
karthikanairp4 <prasannakumarkarthikanair@gmail.com>
2026-07-15 18:16:03 -0500

    Initial commit

---
+++ src/test/java/com/example/thecakestudio/ThecakestudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thecakestudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThecakestudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
